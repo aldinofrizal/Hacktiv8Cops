04a4c5d2b9bce7383d8786777005b69cd8f8ae9f
ItsJoel88 <imanuelnjodi@gmail.com>
2019-09-25 09:49:45 +0700

    test test

--- controller/crime.js
+++ controller/crime.js
@@ -19,4 +19,4 @@ class Controller{
     }
 }
 
-Controller.create()
+module.exports = Controller
---
+++ routes/policeRouter.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const router = express.Router()
+const mdoel = require('../models')
+
+router.get('/')
+
+router.post('/')
+
+
+
+
+module.exports = router
